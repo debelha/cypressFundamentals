0ed132ca10bb74392299c8786bb9b5448f0b1d19
unknown <dleitedias@gmail.com>
2024-09-01 18:13:30 -0300

    add expense registration test

--- cypress/integration/finances.spec.js
+++ cypress/integration/finances.spec.js
@@ -1,10 +1,13 @@
 /// <reference types="cypress" />
 
 context('Dev Finances Agilizei', () => {
-    it('Cadastrar entradas', () => {
-        cy.visit('https://devfinance-agilizei.netlify.app/')
 
-        cy.get('#data-table tbody tr').should('have.length', 0)
+   beforeEach(() => {
+    cy.visit('https://devfinance-agilizei.netlify.app/')
+    cy.get('#data-table tbody tr').should('have.length', 0)
+    })
+
+    it('Cadastrar entradas', () => {
         cy.get('#transaction .button').click() //id + classe
         cy.get('#description').type('Mesada') //id
         cy.get('[name=amount]').type(12) //atributo
@@ -12,7 +15,18 @@ context('Dev Finances Agilizei', () => {
         cy.get('button').contains('Salvar').click() //valor contido
         cy.get('#data-table tbody tr').should('have.length', 1)
     });
+
+    it('Cadastrar saídas', () => {
+        cy.get('#transaction .button').click() //id + classe
+        cy.get('#description').type('Mesada') //id
+        cy.get('[name=amount]').type(-12) //atributo
+        cy.get('[type=date]').type('2024-03-17') //atributo
+        cy.get('button').contains('Salvar').click() //valor contido
+        cy.get('#data-table tbody tr').should('have.length', 1)
+    });
 });
+
+
 
 
 
