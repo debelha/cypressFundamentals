d96a565cf742178feb5deda126949a874cb06489
unknown <dleitedias@gmail.com>
2024-09-01 21:57:25 -0300

    add test for removing entries and expenses

--- cypress/integration/finances.spec.js
+++ cypress/integration/finances.spec.js
@@ -24,6 +24,41 @@ context('Dev Finances Agilizei', () => {
         cy.get('button').contains('Salvar').click() //valor contido
         cy.get('#data-table tbody tr').should('have.length', 1)
     });
+
+    it('Remover entradas e saidas', () => {
+        const entrada = 'Mesada'
+        const saida = "KinderOvo"
+
+        cy.get('#transaction .button').click() //id + classe
+        cy.get('#description').type(entrada) //id
+        cy.get('[name=amount]').type(100) //atributo
+        cy.get('[type=date]').type('2024-03-17') //atributo
+        cy.get('button').contains('Salvar').click() //valor contido
+        cy.get('#data-table tbody tr').should('have.length', 1)
+
+        cy.get('#transaction .button').click() //id + classe
+        cy.get('#description').type(saida) //id
+        cy.get('[name=amount]').type(-35) //atributo
+        cy.get('[type=date]').type('2024-03-17') //atributo
+        cy.get('button').contains('Salvar').click() //valor contido
+        cy.get('#data-table tbody tr').should('have.length', 2)
+
+        //estratégia 1: 
+        cy.get('td.description')
+         .contains(entrada)
+         .parent()
+         .find('img[onclick*=remove]')    
+         .click()
+
+         //estratégia 2:
+         cy.get('td.description')
+         .contains(saida)
+         .siblings()
+         .children('img[onclick*=remove]')
+         .click()
+
+         cy.get('#data-table tbody tr').should('have.length', 0)
+    });
 });
 
 
